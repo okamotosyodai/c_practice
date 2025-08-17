0654e3457b5d0a27c889a32fee24dca5dd0523a4
okamotosyodai <c1417393@st.kanazawa-it.ac.jp>
2025-08-17 23:21:12 +0900

    Remove all files except README.md, hello.c, scope.c

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+//初めてのc言語プログラミング
+int main(void) {
+    printf("Hello World\n");
+    return 0;
+}
---
+++ scope.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+    {
+	int x=123;
+	printf("%d\n",x);
+    }
+    {
+	int x=456;
+	printf("%d\n",x);
+    }
+}
